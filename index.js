732b5f695322d09627f77e5e4a100dbd5a6fdea7
DidasMba <daniel.mbarushi@gmail.com>
2023-10-02 00:29:08 +0300

    complet

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let textParagraph = document.getElementById("text");
+    textParagraph.textContent = "This is really cool!";
+  });
+  
